d55547ff6bb6635e76c08d8ce9209d8026e5112e
mika_laptop_windows <charmikadev@gmail.com>
2021-12-27 14:23:24 +0000

    form complete

--- main.js
+++ main.js
@@ -20,6 +20,27 @@ function checkInputs() {
     } else {
         setSuccessFor(username);
     }
+    if (emailValue === "") {
+        setErrorFor(email, "Email can't be blank.");
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, "Email is not valid.");
+    }
+    if (passwordValue === "") {
+        setErrorFor(password, "Password can't be blank.");
+    } else {
+        setSuccessFor(password);
+    }
+    if (passwordCheckValue === "") {
+        setErrorFor(passwordCheck, "Password can't be blank.");
+    } else if (passwordValue !== passwordCheckValue) {
+        setErrorFor(passwordCheck, "Passwords do not match.")
+    }
+
+
+    else {
+        setSuccessFor(passwordCheck);
+    }
+
 }
 
 function setErrorFor(input, msg) {
@@ -32,4 +53,8 @@ function setErrorFor(input, msg) {
 function setSuccessFor(input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
